e4bdcbc985e7ddd00cd35a138aea00f93121c8e3
karuan <kruan@andrew.cmu.edu>
2018-12-21 00:44:25 -0500

    working on splitting and time outs for splitting

--- scripts/create-workload-simple-deadlock.py
+++ scripts/create-workload-simple-deadlock.py
@@ -10,10 +10,10 @@ outfile = open(sys.argv[1], "w")
 totalTime = 10
 
 #edge a->b appears in index i as rateListStart[i]=a, and rateListEnd[i]=b
-rateListStart = [1,0,2]; 
-rateListEnd = [2,2,0]; 
-rateListAmtTemp = [2,1,2];
-rateListAmt = [50*x for x in rateListAmtTemp]
+rateListStart = [1,0]; 
+rateListEnd = [0,1]; 
+rateListAmtTemp = [10,1];
+rateListAmt = [2*x for x in rateListAmtTemp]
 timeOut = 5.0
 
 for i in range(totalTime):
@@ -21,6 +21,6 @@ for i in range(totalTime):
       for l in range(rateListAmt[k]):
          rate = rateListAmt[k]
          timeStart = i*1.0 + (l*1.0)/(1.0*rate)
-         outfile.write("1 "+str(timeStart)+" "+str(rateListStart[k])+" "+str(rateListEnd[k])+" 0\n") # " + str(timeOut) + " \n");
+         outfile.write("1 "+str(timeStart)+" "+str(rateListStart[k])+" "+str(rateListEnd[k])+" 0 " + str(timeOut) + " \n");
 
 
