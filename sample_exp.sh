d52a3d60e5f19b9e9611b41c29a138fb37c9ed14
vibhaa <vibhaa@mit.edu>
2019-01-09 16:05:20 +0000

    script to run select topologies

---
+++ sample_exp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PATH="benchmarks/circulations/"
+
+prefix=("sw_40_routers" "sf_40_routers"\
+    "sw_400_routers" "sf_400_routers"\
+    "sw_1000_routers" "sf_1000_routers")
+
+arraylength=${#prefix[@]}
+
+for (( i=0; i<${arraylength}; i++));
+do 
+    inifile=$PATH${prefix[i]}_circ.ini
+    network=${prefix[i]}_circ_net
+
+    # run the omnetexecutable with the right parameters
+    ./spiderNet -u Cmdenv -f $inifile -c $network -n $PATH
+done
+
