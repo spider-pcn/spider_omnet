b7b5e1b38a8ce63cce395c57cb8b61e8fa19e582
vibhaa <vibhaa@mit.edu>
2019-01-11 05:26:39 +0000

    adding instructions for using and running experiments

--- scripts/generate_circulation_benchmarks.sh
+++ scripts/generate_circulation_benchmarks.sh
@@ -6,16 +6,14 @@ num_nodes=("2" "3" "4" "5" "10" "0" "0" "40" "60" "80" "100" "200" "400" "600" "
 
 balance=100
 
-#prefix=("two_node" "three_node" "four_node" "five_node" \
-#    "hotnets" "lnd_dec4_2018" "lnd_dec28_2018" \
-#    "sw_40_routers" "sw_60_routers" "sw_80_routers"  \
-#    "sw_100_routers" "sw_200_routers" "sw_400_routers" "sw_600_routers" \
-#    "sw_800_routers" "sw_1000_routers"\
-#    "sf_40_routers" "sf_60_routers" "sf_80_routers"  \
-#    "sf_100_routers" "sf_200_routers" "sf_400_routers" "sf_600_routers" \
-#    "sf_800_routers" "sf_1000_routers")
-#
-prefix=("five_node")
+prefix=("two_node" "three_node" "four_node" "five_node" \
+    "hotnets" "lnd_dec4_2018" "lnd_dec28_2018" \
+    "sw_40_routers" "sw_60_routers" "sw_80_routers"  \
+    "sw_100_routers" "sw_200_routers" "sw_400_routers" "sw_600_routers" \
+    "sw_800_routers" "sw_1000_routers"\
+    "sf_40_routers" "sf_60_routers" "sf_80_routers"  \
+    "sf_100_routers" "sf_200_routers" "sf_400_routers" "sf_600_routers" \
+    "sf_800_routers" "sf_1000_routers")
 
 arraylength=${#prefix[@]}
 PYTHON="/usr/bin/python"
