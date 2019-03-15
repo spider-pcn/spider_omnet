f45b13e6c395790d6d2f8ecc5b8b0d39ad2b784d
vibhaa <vibhaa@mit.edu>
2019-03-15 23:08:19 +0000

    modifying circ benchmark generator

--- scripts/generate_circulation_benchmarks.sh
+++ scripts/generate_circulation_benchmarks.sh
@@ -1,23 +1,56 @@
 #!/bin/bash
-PATH_NAME="../benchmarks/circulations/"
+PATH_NAME="/home/ubuntu/omnetpp-5.4.1/samples/spider_omnet/benchmarks/circulations/"
+GRAPH_PATH="/home/ubuntu/omnetpp-5.4.1/samples/spider_omnet/scripts/figures/"
 
-num_nodes=("2" "2" "3" "4" "5" "5" "5" "0" "0" "10" "20" "40" "60" "80" "100" "200" "400" "600" "800" "1000" \
-    "10" "20" "40" "60" "80" "100" "200" "400" "600" "800" "1000" "40" "10" "20" "30" "40")
+num_nodes=("2" "2" "3" "4" "5" "5" "5" "0" "0" "10" "20" "50" "60" "80" "100" "200" "400" "600" "800" "1000" \
+    "10" "20" "50" "60" "80" "100" "200" "400" "600" "800" "1000" "40" "10" "20" "30" "40")
 
 balance=100
 
 prefix=("two_node_imbalance" "two_node_capacity" "three_node" "four_node" "five_node_hardcoded" \
     "hotnets" "five_line" "lnd_dec4_2018" "lnd_dec28_2018" \
-    "sw_10_routers" "sw_20_routers" "sw_40_routers" "sw_60_routers" "sw_80_routers"  \
+    "sw_10_routers" "sw_20_routers" "sw_50_routers" "sw_60_routers" "sw_80_routers"  \
     "sw_100_routers" "sw_200_routers" "sw_400_routers" "sw_600_routers" \
     "sw_800_routers" "sw_1000_routers"\
     "sf_10_routers" "sf_20_routers" \
-    "sf_40_routers" "sf_60_routers" "sf_80_routers"  \
+    "sf_50_routers" "sf_60_routers" "sf_80_routers"  \
     "sf_100_routers" "sf_200_routers" "sf_400_routers" "sf_600_routers" \
     "sf_800_routers" "sf_1000_routers" "tree_40_routers" "random_10_routers" "random_20_routers"\
     "random_30_routers" "sw_sparse_40_routers")
 
-demand_scale=("5" "10" "15" "20")
+demand_scale=("1" "3" "6" "9") # "60" "90")
+random_init_bal=false
+random_capacity=false
+
+
+#general parameters that do not affect config names
+simulationLength=5100
+statCollectionRate=100
+timeoutClearRate=1
+timeoutEnabled=true
+signalsEnabled=true
+loggingEnabled=false
+
+# scheme specific parameters
+eta=0.025
+alpha=0.2
+kappa=0.025
+updateQueryTime=1.5
+minPriceRate=0.25
+zeta=0.01
+rho=0.04
+tau=10
+normalizer=100
+xi=1
+routerQueueDrainTime=5
+serviceArrivalWindow=300
+
+cp hostNodeBase.ned ${PATH_NAME}
+cp hostNodeWaterfilling.ned ${PATH_NAME}
+cp hostNodeLandmarkRouting.ned ${PATH_NAME}
+cp hostNodePriceScheme.ned ${PATH_NAME}
+cp hostNodeLndBaseline.ned ${PATH_NAME}
+cp routerNode.ned ${PATH_NAME}
 
 arraylength=${#prefix[@]}
 PYTHON="/usr/bin/python"
@@ -26,41 +59,59 @@ mkdir -p ${PATH_NAME}
 # TODO: find the indices in prefix of the topologies you want to run on and then specify them in array
 # adjust experiment time as needed
 #array=( 0 1 4 5 8 19 32)
-array=( 6 )
-for scale in "${demand_scale[@]}"
+array=(11) #10 11 13 22 24)
+for i in "${array[@]}" 
 do
-    for i in "${array[@]}"
-    do 
+    network="${prefix[i]}_circ_net"
+    topofile="${PATH_NAME}${prefix[i]}_topo.txt"
+
+    # identify graph type for topology
+    if [ ${prefix[i]:0:2} == "sw" ]; then
+        graph_type="small_world"
+    elif [ ${prefix[i]:0:2} == "sf" ]; then
+        graph_type="scale_free"
+    elif [ ${prefix[i]:0:4} == "tree" ]; then
+        graph_type="tree"
+    elif [ ${prefix[i]:0:3} == "lnd" ]; then
+        graph_type=${prefix[i]}
+    elif [ ${prefix[i]} == "hotnets" ]; then
+        graph_type="hotnets_topo"
+    elif [ ${prefix[i]:0:6} == "random" ]; then
+        graph_type="random"
+    else
+        graph_type="simple_topologies"
+    fi
+    
+    # set delay amount
+    if [ ${prefix[i]:0:3} == "two" ]; then
+        delay="120"
+    else
+        delay="30"
+    fi
+    
+    # STEP 1: create topology
+    $PYTHON scripts/create_topo_ned_file.py $graph_type\
+            --network-name ${PATH_NAME}$network\
+            --topo-filename $topofile\
+            --num-nodes ${num_nodes[i]}\
+            --balance-per-channel $balance\
+            --separate-end-hosts \
+            --delay-per-channel $delay\
+            --randomize-start-bal $random_init_bal\
+            --random-channel-capacity $random_capacity  
+
+
+    # create workload files and run different demand levels
+    for scale in "${demand_scale[@]}"
+    do
+
         # generate the graph first to ned file
         workloadname="${prefix[i]}_circ_demand${scale}"
-        network="${PATH_NAME}${workloadname}_net"
-        topofile="${PATH_NAME}${prefix[i]}_topo.txt"
         workload="${PATH_NAME}$workloadname"
         inifile="${PATH_NAME}${workloadname}_default.ini"
         payment_graph_topo="custom"
-
-        if [ ${prefix[i]:0:2} == "sw" ]; then
-            graph_type="small_world"
-        elif [ ${prefix[i]:0:2} == "sf" ]; then
-            graph_type="scale_free"
-        elif [ ${prefix[i]:0:4} == "tree" ]; then
-            graph_type="tree"
-        elif [ ${prefix[i]:0:3} == "lnd" ]; then
-            graph_type=$prefix{i}
-        elif [ ${prefix[i]} == "hotnets" ]; then
-            graph_type="hotnets_topo"
-        elif [ ${prefix[i]:0:6} == "random" ]; then
-            graph_type="random"
-        else
-            graph_type="simple_topologies"
-        fi
         
-        if [ ${prefix[i]:0:3} == "two" ]; then
-            delay="120"
-        else
-            delay="30"
-        fi
-
+        # figure out payment graph/workload topology
         if [ ${prefix[i]:0:9} == "five_line" ]; then
             payment_graph_topo="simple_line"
         elif [ ${prefix[i]:0:4} == "five" ]; then
@@ -74,34 +125,17 @@ do
         echo $inifile
         echo $graph_type
 
-        
-        $PYTHON create_topo_ned_file.py $graph_type\
-                --network-name $network\
-                --topo-filename $topofile\
-                --num-nodes ${num_nodes[i]}\
-                --balance-per-channel $balance\
-                --separate-end-hosts \
-                --delay-per-channel $delay\
-                #--randomize-start-bal
-
-
-        # create transactions corresponding to this experiment run
-        $PYTHON create_workload.py $workload poisson \
+        # STEP 2: create transactions corresponding to this experiment run
+        $PYTHON scripts/create_workload.py $workload poisson \
                 --graph-topo $payment_graph_topo \
                 --payment-graph-dag-percentage 0\
                 --topo-filename $topofile\
-                --experiment-time 4000 \
+                --experiment-time $simulationLength \
                 --balance-per-channel $balance\
                 --generate-json-also \
                 --timeout-value 5 \
-                --scale-amount $scale
-
-        # create the ini file
-        $PYTHON create_ini_file.py \
-                --network-name $network\
-                --topo-filename ${topofile:3} \
-                --workload-filename ${workload:3}_workload.txt\
-                --ini-filename $inifile
+                --kaggle-size \
+                --scale-amount $scale 
     done
 done
 
