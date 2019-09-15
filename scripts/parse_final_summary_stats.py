55a656e153e0ef03992fab3a5692e961b54fcfe6
vibhaa <vibhaa@mit.edu>
2019-09-15 18:51:43 +0000

    parsing now includes scheduling alg

--- scripts/parse_final_summary_stats.py
+++ scripts/parse_final_summary_stats.py
@@ -24,6 +24,9 @@ parser.add_argument('--demand',
 parser.add_argument('--path-type-list',
         nargs="*",
         help='types of paths to collect data for', default=["shortest"])
+parser.add_argument('--scheduling-alg-list',
+        nargs="*",
+        help='scheduling algorithms to collect info for', default=[None])
 parser.add_argument('--queue-threshold-list',
         nargs="*",
         help='queue thresholds to collect info for', default=[None])
@@ -54,6 +57,7 @@ scheme_list = args.scheme_list
 path_num_list = args.path_num_list
 queue_threshold_list = args.queue_threshold_list
 dag_percent_list = args.dag_percent_list
+scheduling_algorithms = args.scheduling_alg_list
 
 output_file = open(GGPLOT_DATA_DIR + args.save, "w+")
 if args.payment_graph_type == "circ":
@@ -61,7 +65,8 @@ if args.payment_graph_type == "circ":
 else:
     output_file.write("Scheme,Credit,DAGAmt,")
 
-output_file.write("Topo,CreditType,NumPaths,PathType,Threshold,SuccRatio,SuccRatioMin,SuccRatioMax,SuccVolume," + \
+output_file.write("Topo,CreditType,NumPaths,PathType,SchedulingAlg," + \
+        "Threshold,SuccRatio,SuccRatioMin,SuccRatioMax,SuccVolume," + \
         "SuccVolumeMin," +\
         "SuccVolumeMax,CompTime,CompTimeMin,CompTimeMax\n")
 
@@ -92,64 +97,70 @@ for credit in credit_list:
             for queue_threshold in queue_threshold_list:
                 for num_paths in path_num_list:
                     for percent in dag_percent_list:
-                        succ_ratios, succ_vols,comp_times = [], [], []
-                        for run_num in range(0, args.num_max  + 1):
-                            if args.payment_graph_type == "circ" or percent == '0':
-                                file_name = topo + str(credit) + "_circ" + str(run_num)
+                        for alg in scheduling_algorithms:
+                            succ_ratios, succ_vols,comp_times = [], [], []
+                            for run_num in range(0, args.num_max  + 1):
+                                if args.payment_graph_type == "circ" or percent == '0':
+                                    file_name = topo + str(credit) + "_circ" + str(run_num)
+                                else:
+                                    file_name = topo + "_dag" + str(percent) + "_" + str(credit) + "_num" + \
+                                            str(run_num) 
+                                
+                                file_name += "_delay" + str(delay) + "_demand" + str(demand) + "_" + scheme + \
+                                        "_" + path_type 
+
+                                if scheme != "shortestPath":
+                                    file_name += "_" + str(num_paths) 
+                                if alg is not None:
+                                    file_name += "_" + alg
+                                
+                                if queue_threshold is not None and percent != '0' and scheme == "DCTCPQ":
+                                    file_name += "_qd" + str(queue_threshold)
+
+
+                                file_name += "_summary.txt"
+                                
+                                try: 
+                                    with open(SUMMARY_DIR + file_name) as f:
+                                        for line in f:
+                                            if line.startswith("Success ratio"):
+                                                succ_ratio = float(line.split(" ")[4])
+                                            elif line.startswith("Success volume"):
+                                                succ_volume = float(line.split(" ")[5])
+                                            elif line.startswith("Avg completion time"):
+                                                comp_time = float(line.split(" ")[3][:-1])
+                                        succ_ratios.append(succ_ratio * 100)
+                                        succ_vols.append(succ_volume * 100)
+                                        comp_times.append(comp_time)
+                                except IOError:
+                                    print "error with " , file_name
+                                    continue
+
+                            if "lndtopo" in args.save and "lnd_credit" in args.save:
+                                capacity = int(credit) * 650
+                            elif "lndnewtopo" in args.save and "lnd_credit" in args.save:
+                                capacity = int(credit) * 422
                             else:
-                                file_name = topo + "_dag" + str(percent) + "_" + str(credit) + "_num" + \
-                                        str(run_num) 
+                                capacity = int(credit)
+
+
+
                             
-                            file_name += "_delay" + str(delay) + "_demand" + str(demand) + "_" + scheme + \
-                                    "_" + path_type 
+                            if len(succ_ratios) > 0:
+                                if args.payment_graph_type == "circ":
+                                    output_file.write(SCHEME_CODE[scheme] + "," + str(capacity) +  ",")
+                                else:
+                                    output_file.write(SCHEME_CODE[scheme] + "," + str(capacity) +  "," + \
+                                            str(PERCENT_MAPPING[percent]) + ",")
 
-                            if scheme != "shortestPath":
-                                file_name += "_" + str(num_paths) 
-                            
-                            if queue_threshold is not None and percent != '0' and scheme == "DCTCPQ":
-                                file_name += "_qd" + str(queue_threshold)
-                            file_name += "_summary.txt"
-                            
-                            try: 
-                                with open(SUMMARY_DIR + file_name) as f:
-                                    for line in f:
-                                        if line.startswith("Success ratio"):
-                                            succ_ratio = float(line.split(" ")[4])
-                                        elif line.startswith("Success volume"):
-                                            succ_volume = float(line.split(" ")[5])
-                                        elif line.startswith("Avg completion time"):
-                                            comp_time = float(line.split(" ")[3][:-1])
-                                    succ_ratios.append(succ_ratio * 100)
-                                    succ_vols.append(succ_volume * 100)
-                                    comp_times.append(comp_time)
-                            except IOError:
-                                print "error with " , file_name
-                                continue
-
-                        if "lndtopo" in args.save and "lnd_credit" in args.save:
-                            capacity = int(credit) * 650
-                        elif "lndnewtopo" in args.save and "lnd_credit" in args.save:
-                            capacity = int(credit) * 422
-                        else:
-                            capacity = int(credit)
-
-
-
-                        
-                        if len(succ_ratios) > 0:
-                            if args.payment_graph_type == "circ":
-                                output_file.write(SCHEME_CODE[scheme] + "," + str(capacity) +  ",")
-                            else:
-                                output_file.write(SCHEME_CODE[scheme] + "," + str(capacity) +  "," + \
-                                        str(PERCENT_MAPPING[percent]) + ",")
-
-                            output_file.write(topo_type + "," + credit_type + "," \
-                                    + str(num_paths) + "," \
-                                + str(path_type) + "," \
-                                + str(queue_threshold) + "," \
-                                + ("%f,%f,%f,%f,%f,%f,%f,%f,%f\n" % (stat.mean(succ_ratios), min(succ_ratios), \
-                                max(succ_ratios), stat.mean(succ_vols), min(succ_vols),  max(succ_vols), \
-                                stat.mean(comp_times), min(comp_times), max(comp_times))))
+                                output_file.write(topo_type + "," + credit_type + "," \
+                                        + str(num_paths) + "," \
+                                    + str(path_type) + "," \
+                                    + str(alg) + "," \
+                                    + str(queue_threshold) + "," \
+                                    + ("%f,%f,%f,%f,%f,%f,%f,%f,%f\n" % (stat.mean(succ_ratios), min(succ_ratios), \
+                                    max(succ_ratios), stat.mean(succ_vols), min(succ_vols),  max(succ_vols), \
+                                    stat.mean(comp_times), min(comp_times), max(comp_times))))
     
     if args.payment_graph_type == 'dag':
         for percent in dag_percent_list:
