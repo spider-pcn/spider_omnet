ba8c03a01df6b69e7a8e11c1b92075aaf8e93d61
karuan <kathleenruanse@gmail.com>
2019-10-27 11:43:00 -0400

    added DestNodeStruct file

---
+++ structs/DestNodeStruct.h
@@ -0,0 +1,10 @@
+using namespace std;
+using namespace omnetpp;
+
+
+class DestNodeStruct{
+public:
+    vector<tuple<int, double, routerMsg*,  Id, simtime_t >> queuedTransUnits; //make_heap in initialization
+    double queueSizeSum = 0;
+    double totalAmtInQueue = 0; // total amount in the queue
+};
