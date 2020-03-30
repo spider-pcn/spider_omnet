436129e3602683ac28cf610f9c29604d8e6168c1
vibhaa <vibhaa@mit.edu>
2020-03-30 21:43:08 +0000

    scheduling alg, types of paths scripts

---
+++ eval-scripts/run_types_of_paths.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# NOTE: while a single OMNET experiment uses a single core,
+# the memory consumption of each one of these runs can be on the order of 
+# 50 GB for small world/ scale free and 100 GB for the LND topology
+# Do not use this script as is, without first judging the capabilities of
+# your system to run experiments in parallel
+
+# small world topology, kaggle data for transactions
+# make sure all path types are avilable first for all topologies
+for path_type in "shortest" "kspYen" "oblivious" "heuristic" #"widest"
+do
+    ./run_experiment_set.sh \
+        --prefix=sw_50_routers_lndCap \
+        --workload-prefix=sw_50_routers \
+        --exp-type=circ \
+        --routing-scheme=DCTCPQ \
+        --num_start=0 --num_end=4 \
+        --balance-list="900 1350 2750 4000 8750" \
+        --path-choice=${path_type} \
+        --num-paths=4 \
+        --demand-scale="3" \
+        --scheduling-alg=LIFO &
+done
+wait
+
+# scale free topology, kaggle transaction data
+for path_type in "shortest" "kspYen" "oblivious" "heuristic" #"widest"
+do
+    ./run_experiment_set.sh \
+        --prefix=sf_50_routers_lndCap \
+        --workload-prefix=sf_50_routers \
+        --exp-type=circ \
+        --routing-scheme=DCTCPQ \
+        --num_start=0 --num_end=4 \
+        --balance-list="900 1350 2750 4000 8750" \
+        --path-choice=${path_type} \
+        --num-paths=4 \
+        --demand-scale="3" \
+        --scheduling-alg=LIFO &
+done
+wait
+
+# lnd topology, kaggle transaction data
+for path_type in "shortest" "kspYen" "oblivious" "heuristic" #"widest"
+do
+    ./run_experiment_set.sh \
+        --prefix=lndnewtopo \
+        --workload-prefix=lndnewtopo \
+        --exp-type=circ \
+        --routing-scheme=DCTCPQ \
+        --num_start=0 --num_end=4 \
+        --balance-list="10 20 40 80 160" \
+        --path-choice=${path_type} \
+        --num-paths=4 \
+        --demand-scale="3" \
+        --scheduling-alg=LIFO &
+done
+wait
