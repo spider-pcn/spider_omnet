4a8d42a33eb313a77fd57acca0a37a790cff2cab
vibhaa <vibhaa@mit.edu>
2020-03-30 22:40:10 +0000

    separting out the copying over of files per benchmark directory

--- eval-scripts/setup.sh
+++ eval-scripts/setup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-PATH_NAME=$HOME"/"${OMNET}"/samples/spider_omnet/benchmarks/circulations/"
+PATH_PREFIX=$HOME"/"${OMNET}"/samples/spider_omnet/benchmarks/"
+CIRC_PATH_NAME=${PATH_PREFIX}/"circulations/"
 GRAPH_PATH=$HOME"/"${OMNET}"/samples/spider_omnet/scripts/figures/"
 DEFAULT_PATH=$HOME"/"${OMNET}"/samples/spider_omnet"
 
-for suffix in "Base" "Waterfilling" "LndBaseline" "PriceScheme" "DCTCP" "Celer"
-do
-    cp ${DEFAULT_PATH}/hostNode${suffix}.ned ${PATH_NAME}
-    cp ${DEFAULT_PATH}/routerNode${suffix}.ned ${PATH_NAME}
-done
-cp ${DEFAULT_PATH}/hostNodeLandmarkRouting.ned ${PATH_NAME}
-cp ${DEFAULT_PATH}/routerNodeDCTCPBal.ned ${PATH_NAME}
+function setup {
+    path_name=$1
+    mkdir -p ${path_name}
+    for suffix in "Base" "Waterfilling" "LndBaseline" "PriceScheme" "DCTCP" "Celer"
+    do
+        cp ${DEFAULT_PATH}/hostNode${suffix}.ned ${path_name}
+        cp ${DEFAULT_PATH}/routerNode${suffix}.ned ${path_name}
+    done
+    cp ${DEFAULT_PATH}/hostNodeLandmarkRouting.ned ${path_name}
+    cp ${DEFAULT_PATH}/routerNodeDCTCPBal.ned ${path_name}
+}
 
-mkdir -p ${PATH_NAME}
