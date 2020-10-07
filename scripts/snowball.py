6f7a25ead8130f56a009d155d136c02f5f380752
vibhaa <vibhaa@mit.edu>
2020-10-07 13:43:55 -0400

    adjusting tabs/whitespaces

--- scripts/snowball.py
+++ scripts/snowball.py
@@ -15,71 +15,71 @@ import numpy as np
 random.seed(SEED)
 
 def sample_ngbr(graph, ngbr_set, v_set, k):
-	""" for each vertex in ngbr_set sample k random neighbors
-	in graph """
-	
-	new_ngbr_set = set() 
-	new_edge_set = set()
+    """ for each vertex in ngbr_set sample k random neighbors
+    in graph """
+    
+    new_ngbr_set = set() 
+    new_edge_set = set()
 
-	for v in ngbr_set:
-		ngbr = [u for u in graph.neighbors(v)]
-		if len(ngbr) > k:
-			ngbr_sample = set(random.sample(ngbr, k))
-		else:
-			ngbr_sample = set(ngbr)
-		new_ngbr_set = new_ngbr_set | ngbr_sample
-		new_edge_set = new_edge_set | {(u, v) for u in ngbr_sample}
-	
-	new_ngbr_set = new_ngbr_set - v_set
-	return new_ngbr_set, new_edge_set
+    for v in ngbr_set:
+        ngbr = [u for u in graph.neighbors(v)]
+        if len(ngbr) > k:
+            ngbr_sample = set(random.sample(ngbr, k))
+        else:
+            ngbr_sample = set(ngbr)
+        new_ngbr_set = new_ngbr_set | ngbr_sample
+        new_edge_set = new_edge_set | {(u, v) for u in ngbr_sample}
+    
+    new_ngbr_set = new_ngbr_set - v_set
+    return new_ngbr_set, new_edge_set
 
 
 
 def snowball_sample(graph, init_seed, max_sample_size=100, k=4):
-	""" sample a subgraph no more than max_sample_size starting with 
-	vertices in init_seed """
+    """ sample a subgraph no more than max_sample_size starting with 
+    vertices in init_seed """
 
-	if len(graph.nodes()) <= max_sample_size:
-		return graph
+    if len(graph.nodes()) <= max_sample_size:
+        return graph
 
-	assert len(init_seed) <= max_sample_size
+    assert len(init_seed) <= max_sample_size
 
-	v_set = copy.deepcopy(init_seed)
-	e_set = set()
-	old_v_set = v_set
-	old_e_set = e_set
-	ngbr_set = copy.deepcopy(init_seed)
+    v_set = copy.deepcopy(init_seed)
+    e_set = set()
+    old_v_set = v_set
+    old_e_set = e_set
+    ngbr_set = copy.deepcopy(init_seed)
 
-	while len(v_set) <= max_sample_size:
-		new_ngbr_set, new_edge_set = sample_ngbr(graph, ngbr_set, v_set, k)
-		old_v_set = v_set.copy()
-		old_e_set = e_set.copy()
-		v_set = v_set | new_ngbr_set
-		e_set = e_set | new_edge_set
-		ngbr_set = new_ngbr_set
+    while len(v_set) <= max_sample_size:
+        new_ngbr_set, new_edge_set = sample_ngbr(graph, ngbr_set, v_set, k)
+        old_v_set = v_set.copy()
+        old_e_set = e_set.copy()
+        v_set = v_set | new_ngbr_set
+        e_set = e_set | new_edge_set
+        ngbr_set = new_ngbr_set
 
-	sampled_graph = nx.Graph()
-	sampled_graph.add_nodes_from(list(old_v_set))
-	sampled_graph.add_edges_from(list(old_e_set))
+    sampled_graph = nx.Graph()
+    sampled_graph.add_nodes_from(list(old_v_set))
+    sampled_graph.add_edges_from(list(old_e_set))
 
-	for e in sampled_graph.edges():
-		sampled_graph.edges[e]['capacity'] = graph.edges[e]['capacity']
+    for e in sampled_graph.edges():
+        sampled_graph.edges[e]['capacity'] = graph.edges[e]['capacity']
 
-	return sampled_graph
+    return sampled_graph
 
 
 
 def prune_deg_one_nodes(sampled_graph):
-	""" prune out degree one nodes from graph """
-	deg_one_nodes = []
-	for v in sampled_graph.nodes():
-		if sampled_graph.degree(v) == 1:
-			deg_one_nodes.append(v)
+    """ prune out degree one nodes from graph """
+    deg_one_nodes = []
+    for v in sampled_graph.nodes():
+        if sampled_graph.degree(v) == 1:
+            deg_one_nodes.append(v)
 
-	for v in deg_one_nodes:
-		sampled_graph.remove_node(v)
+    for v in deg_one_nodes:
+        sampled_graph.remove_node(v)
 
-	return sampled_graph
+    return sampled_graph
 
 
 
@@ -91,54 +91,49 @@ def write_capacities_to_file(filename, capacities):
 
 
 #lnd_file_list = ["lnd_dec4_2018", "lnd_dec28_2018"]
-lnd_file_list = ["lnd_july15_2019"]
+#lnd_file_list = ["lnd_july15_2019"]
+lnd_file_list = ["clightning_oct5_2020"]
 for filename in lnd_file_list: 
-	graph = nx.read_edgelist(LND_FILE_PATH + filename + ".edgelist")
+    graph = nx.read_edgelist(LND_FILE_PATH + filename + ".edgelist")
 
-	rename_dict = {v: int(str(v)) for v in graph.nodes()}
-	graph = nx.relabel_nodes(graph, rename_dict)
+    rename_dict = {v: int(str(v)) for v in graph.nodes()}
+    graph = nx.relabel_nodes(graph, rename_dict)
 
-        # convert all capacities to EUROS
-	for e in graph.edges():
-                edge_cap = int(str(graph.edges[e]['capacity'])) / SAT_TO_EUR
-                if edge_cap < 25:
-                    edge_cap = 25
-		graph.edges[e]['capacity'] = edge_cap
-	
-        init_seed = {784, 549}
+    # convert all capacities to EUROS
+    count = 0
+    for e in graph.edges():
+        edge_cap = round(graph.edges[e]['capacity'] / SAT_TO_EUR)
+        if edge_cap < 10:
+            edge_cap = 10
+            count += 1
+        graph.edges[e]['capacity'] = edge_cap
+    print("massaged", count, "out of", len(graph.edges()), "edges")
+    
+    init_seed = {784, 549}
 
-	""" max_sample_size is the maximum size of sampled graph. Returned graph 
-	might be smaller than that. k is how many neighbors to sample (by each node)
-	in each round """
-	sampled_graph = snowball_sample(graph, init_seed, max_sample_size=300, k=8)
+    """ max_sample_size is the maximum size of sampled graph. Returned graph 
+    might be smaller than that. k is how many neighbors to sample (by each node)
+    in each round """
+    sampled_graph = snowball_sample(graph, init_seed, max_sample_size=500, k=8)
 
-	""" prune out degree one nodes until there are no degree one nodes """
-	graph_size = len(sampled_graph.nodes()) + 1
-	while graph_size > len(sampled_graph.nodes()):
-		graph_size = len(sampled_graph.nodes())
-		sampled_graph = prune_deg_one_nodes(sampled_graph)
+    """ prune out degree one nodes until there are no degree one nodes """
+    graph_size = len(sampled_graph.nodes()) + 1
+    while graph_size > len(sampled_graph.nodes()):
+        graph_size = len(sampled_graph.nodes())
+        sampled_graph = prune_deg_one_nodes(sampled_graph)
 
-        # convert all capacities to EUROS, doesn't matter if you do before or after snowball sampling
-        count = 0
-	for e in sampled_graph.edges():
-                edge_cap = int(str(graph.edges[e]['capacity'])) / SAT_TO_EUR
-                if edge_cap < 25:
-                    edge_cap = 25
-                    count += 1
-		sampled_graph.edges[e]['capacity'] = edge_cap
-        print("massaged", count, "out of", len(sampled_graph.edges()), "edges")
-	
-        """ make all node numbers start from 0 """
-	numbered_graph = nx.convert_node_labels_to_integers(sampled_graph)
-	print("graph size: ", numbered_graph.number_of_nodes(), " nodes" , \
-			numbered_graph.number_of_edges(), " edges")
+    
+    """ make all node numbers start from 0 """
+    numbered_graph = nx.convert_node_labels_to_integers(sampled_graph)
+    print("graph size: ", numbered_graph.number_of_nodes(), " nodes" , \
+            numbered_graph.number_of_edges(), " edges")
 
-	nx.write_edgelist(numbered_graph, LND_FILE_PATH + filename + "_reducedsize" + ".edgelist")
+    nx.write_edgelist(numbered_graph, LND_FILE_PATH + filename + "_reducedsize" + ".edgelist")
         
-        capacities = nx.get_edge_attributes(sampled_graph, 'capacity')
-        capacities = [float(c) for c in list(capacities.values())]
-        write_capacities_to_file("lnd_july15_data_min25", capacities)
-        #plt.hist(capacities, bins=100, normed=True, cumulative=True)
-        print(np.mean(np.array(capacities)), "stddev" , np.std(np.array(capacities),), min(capacities), \
-                np.median(np.array(capacities)), np.percentile(np.array(capacities), 25))
-        #plt.show()
+    capacities = nx.get_edge_attributes(sampled_graph, 'capacity')
+    capacities = [float(c) for c in list(capacities.values())]
+    write_capacities_to_file(filename + "_data_min25", capacities)
+    #plt.hist(capacities, bins=100, normed=True, cumulative=True)
+    print(np.mean(np.array(capacities)), "stddev" , np.std(np.array(capacities),), min(capacities), \
+            np.median(np.array(capacities)), np.percentile(np.array(capacities), 25))
+    #plt.show()
